c1ee28086cd00bae9a1063016232774634c35aa1
ifrah haider <ifrahhaider@aptechnorth.edu.pk>
2024-07-18 14:33:18 +0500

    my commit

---
+++ ab/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var s = new List<Student>{
+
+            new Student { Id = 12, Name = "Aqib", Age =18, standard="10" },
+
+            new Student { Id = 12, Name = "Taha", Age =18, standard="10" },
+
+            new Student { Id = 12, Name = "Bilal", Age =18, standard="10" },
+
+            new Student { Id = 12, Name = "Umair", Age =18, standard="10" },
+
+            };
+            TempData["model_data"] = s;
+            return View();
+        }
+  
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ab/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ab.Models
+{
+    public class Student
+    {
+         public int Id { get; set; }
+         public string? Name { get; set; }
+         public int Age { get; set; }
+         public string? standard {  get; set; }
+
+    }
+}
